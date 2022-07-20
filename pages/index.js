5f7a4d55fcc23f02fcf72b4728aa36bef5b7b3ae
Lainey <lainey@infograte.com>
2022-07-20 12:22:08 +0200

    updating index with components/styling

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
-// import Head from "next/head";
-// import Image from "next/image";
-// import styles from "../styles/Home.module.css";
-import Card from "../components/Card";
+import Hero from "../components/Hero";
 import Nav from "../components/Nav";
 import AboutMe from "../components/AboutMe";
-// import Ripple from "../components/Ripple";
+import Projects from "../components/Projects";
+import Connect from "../components/Connect";
 import React from "react";
+import styles from "../styles/Index.module.css";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>
-            {/* <Ripple /> */}
-            <Card src="/header.jpeg" />
-            <Nav />
+        <div className={styles.viewWindow}>
+            <div className={styles.content}>
+                <div className={styles.pic}>
+                    <Image
+                        src="/header.jpeg"
+                        alt="landscape"
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
 
-            <AboutMe src="/peru.jpg" />
+                <Nav />
+                <Hero />
+                <AboutMe />
+                <Projects />
+                <Connect />
+            </div>
         </div>
     );
 }
