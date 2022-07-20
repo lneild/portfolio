c1be99b2edbaf115c8597ea96e8641817a81ad9b
Lainey <lainey@infograte.com>
2022-07-20 12:23:26 +0200

    adding popup.js

---
+++ components/Popup.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Popup.module.css";
+import Button from "../components/Button";
+import Polaroid from "./Polaroid";
+
+export default function Popup(props) {
+    console.log(props);
+    return (
+        <div className={styles.viewWindow}>
+            <div className={styles.card}>
+                <div className={styles.title}>{props.title}</div>
+                <div className={styles.content}>
+                    <div className={styles.photo}>
+                        <Polaroid
+                            title={props.picTitle}
+                            date={props.picDate}
+                            src={props.src}
+                        />
+                    </div>
+                    <div className={styles.description}>
+                        {props.description}
+                    </div>
+                </div>
+                <div
+                    className={styles.button}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        props.handleClose();
+                    }}
+                >
+                    <Button text="CLOSE" />
+                </div>
+            </div>
+        </div>
+    );
+}
