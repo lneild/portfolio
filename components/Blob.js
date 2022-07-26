a2b07b2179448d32c24b08aca2fd8a933a2754b8
Lainey <lainey@infograte.com>
2022-07-26 13:58:06 +0200

    adding blob component

--- components/Blob.js
+++ components/Blob.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Blob.module.css";
+import CodePopup from "../components/CodePopup";
+
+const revenue = `
+select
+  sum(p.payable_price) as amount,
+  date_trunc('{{frequency}}', p.paid_at :: date) as date
+from
+  payments p
+  inner join finalized_payments fp on p.id = fp.payment_id
+where
+  p.paid_at between '{{ start }}'
+  and '{{ end }}'
+group by
+  date_trunc('{{frequency}}', p.paid_at :: date)`;
 
 export default function Blob(props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClose = () => {
+        setIsOpen(false);
+    };
+
+    const handleOpen = () => {
+        setIsOpen(true);
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            document.getElementById("header").style.display = "none";
+        } else {
+            document.getElementById("header").style.display = "flex";
+        }
+    }, [isOpen]);
+
     return (
-        <div className={styles.blob}>
-            <div className={styles.text}>{props.description}</div>
+        <div>
+            {isOpen && <CodePopup handleClose={handleClose} code={revenue} />}
+            <div className={styles.blob}>
+                <div className={styles.text}>{props.description}</div>
+            </div>
         </div>
     );
 }
