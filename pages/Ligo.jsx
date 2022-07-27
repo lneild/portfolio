f4e44ef6c2ec1d8660e560bb4a02e972b4730732
Lainey <lainey@infograte.com>
2022-07-27 16:06:51 +0200

    updating logo in background effect

--- pages/Ligo.jsx
+++ pages/Ligo.jsx
@@ -20,13 +20,11 @@ export default function Ligo() {
     const handleHoverIn = (tool) => {
         console.log("in");
         setIsHovered(tool);
-        console.log("hi" + logoData[tool]);
     };
 
-    const pictureCode = (logo) => {
-        console.log(logo);
+    const PictureCode = ({ enabled, logo }) => {
         return (
-            <div className={styles.photo}>
+            <div className={`${styles.photo} ${!enabled ? styles.hidden : ""}`}>
                 <Image
                     src={logo}
                     alt={logo}
@@ -38,48 +36,51 @@ export default function Ligo() {
         );
     };
 
-    const jobDescription = (
-        <p>
-            Ligo is a legal platform to start, run and protect businesses by
-            allowing them to easily create, sign, notarize, and manage legal
-            paperwork located in Amsterdam, NL. While I worked there, In
-            general, I analyzed data and served as link between development team
-            and growth team. I primarily coded in
-            <span
-                className={styles.keyTool}
-                onMouseOver={() => handleHoverIn("sql")}
-                onMouseLeave={() => handleHoverOut()}
-            >
-                {" "}
-                SQL{" "}
-            </span>
-            to display data in an effort to help the marketing team create their
-            stategy. Most of my tasks were assigned through
-            <span
-                className={styles.keyTool}
-                onMouseOver={() => handleHoverIn("notion")}
-                onMouseLeave={() => handleHoverOut()}
-            >
-                {" "}
-                Notion{" "}
-            </span>
-            and I coded in a Redashdomain called Stats. This tool allowed me to
-            easily publish all visals to public dashboards, visible to all
-            members at the company. In adding to coding SQL queries, I also
-            utilized
-            <span
-                className={styles.keyTool}
-                onMouseOver={() => handleHoverIn("ga")}
-                onMouseLeave={() => handleHoverOut()}
-            >
-                {" "}
-                Google Anayltics{" "}
-            </span>
-            to anaylze page traffic and diagnose the root of troughs in revenue
-            so that the growth team could try and course correct the company.
-            {isHovered && pictureCode(logoData[isHovered])}
-        </p>
-    );
+    const JobDescription = () => {
+        return (
+            <p>
+                Ligo is a legal platform to start, run and protect businesses by
+                allowing them to easily create, sign, notarize, and manage legal
+                paperwork located in Amsterdam, NL. While I worked there, In
+                general, I analyzed data and served as link between development
+                team and growth team. I primarily coded in
+                <span
+                    className={styles.keyTool}
+                    onMouseEnter={() => handleHoverIn("sql")}
+                    onMouseLeave={() => handleHoverOut()}
+                >
+                    {" "}
+                    SQL{" "}
+                </span>
+                to display data in an effort to help the marketing team create
+                their stategy. Most of my tasks were assigned through
+                <span
+                    className={styles.keyTool}
+                    onMouseEnter={() => handleHoverIn("notion")}
+                    onMouseLeave={() => handleHoverOut()}
+                >
+                    {" "}
+                    Notion{" "}
+                </span>
+                and I coded in a Redashdomain called Stats. This tool allowed me
+                to easily publish all visals to public dashboards, visible to
+                all members at the company. In adding to coding SQL queries, I
+                also utilized
+                <span
+                    className={styles.keyTool}
+                    onMouseEnter={() => handleHoverIn("ga")}
+                    onMouseLeave={() => handleHoverOut()}
+                >
+                    {" "}
+                    Google Anayltics{" "}
+                </span>
+                to anaylze page traffic and diagnose the root of troughs in
+                revenue so that the growth team could try and course correct the
+                company.
+            </p>
+        );
+    };
+
     return (
         <div id="ligo" className={styles.ligo}>
             <div id="header" className={styles.header}>
@@ -95,7 +96,21 @@ export default function Ligo() {
             </div>
             <div className={styles.background}>
                 <div className={styles.workTitle}>data science intern</div>
-                <div className={styles.description}>{jobDescription}</div>
+                <div className={styles.description}>
+                    <JobDescription />
+                    <PictureCode
+                        logo={logoData.sql}
+                        enabled={isHovered === "sql"}
+                    />
+                    <PictureCode
+                        logo={logoData.notion}
+                        enabled={isHovered === "notion"}
+                    />
+                    <PictureCode
+                        logo={logoData.ga}
+                        enabled={isHovered === "ga"}
+                    />
+                </div>
                 <div className={styles.workTitle}>my work</div>
             </div>
             <div className={styles.myWork}>
