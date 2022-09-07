21c7868af31b9f66bb01cd14fee0847578232dd2
Lainey <lainey@infograte.com>
2022-09-07 18:02:45 -0400

    update contact page

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -6,101 +6,51 @@ import Link from "next/link";
 export default function Connect() {
     return (
         <div id="connect">
-            {/* <div className={styles.pic}>
-                <Image
-                    src="/ocean.jpg"
-                    alt="Ocean"
-                    layout="fill"
-                    objectFit="cover"
-                    priority
-                />
-            </div> */}
             <div className={styles.connect}>
-                <div className={styles.title}>LETS CONNECT</div>
                 <div className={styles.content}>
                     <div className={styles.socialContainer}>
-                        <Link href="mailto:example@example.com">
+                        <Link href="mailto:laineneild@gmial.com">
                             <div className={styles.social}>
-                                <div className={styles.icon1}>
-                                    <Image
-                                        src="/mail1.png"
-                                        alt="mail"
-                                        width="120"
-                                        height="120"
-                                    />
-                                </div>
-
-                                <div className={styles.icon2}>
-                                    <Image
-                                        src="/mail2.png"
-                                        alt="mail"
-                                        width="120"
-                                        height="120"
-                                    />
-                                </div>
+                                <Image
+                                    src="/mail.png"
+                                    alt="mail"
+                                    width="30"
+                                    height="30"
+                                />
                             </div>
                         </Link>
                         <Link href="https://github.com/lneild">
                             <div className={styles.social}>
-                                <div className={styles.icon1}>
-                                    <Image
-                                        src="/github1.png"
-                                        alt="github"
-                                        width="140"
-                                        height="140"
-                                    />
-                                </div>
-                                <div className={styles.icon2}>
-                                    <Image
-                                        src="/github2.png"
-                                        alt="github"
-                                        width="140"
-                                        height="140"
-                                    />
-                                </div>
+                                <Image
+                                    src="/github.png"
+                                    alt="github"
+                                    width="30"
+                                    height="30"
+                                />
                             </div>
                         </Link>
                         <Link href="https://www.linkedin.com/in/lainey-neild/">
                             <div className={styles.social}>
-                                <div className={styles.icon1}>
-                                    <Image
-                                        src="/linkedin1.png"
-                                        alt="linkedin"
-                                        width="140"
-                                        height="140"
-                                    />
-                                </div>
-                                <div className={styles.icon2}>
-                                    <Image
-                                        src="/linkedin2.png"
-                                        alt="linkedin"
-                                        width="140"
-                                        height="140"
-                                    />
-                                </div>
+                                <Image
+                                    src="/linkedin.png"
+                                    alt="linkedin"
+                                    width="30"
+                                    height="30"
+                                />
                             </div>
                         </Link>
                         <Link href="tel:+19144133732">
                             <div className={styles.social}>
-                                <div className={styles.icon1}>
-                                    <Image
-                                        src="/phone1.png"
-                                        alt="phone"
-                                        width="120"
-                                        height="120"
-                                    />
-                                </div>
-                                <div className={styles.icon2}>
-                                    <Image
-                                        src="/phone2.png"
-                                        alt="phone"
-                                        width="120"
-                                        height="120"
-                                    />
-                                </div>
+                                <Image
+                                    src="/phone.png"
+                                    alt="phone"
+                                    width="30"
+                                    height="30"
+                                />
                             </div>
                         </Link>
                     </div>
+                    <div className={styles.title}>Lainey Neild 2022 ©</div>
                 </div>
             </div>
         </div>
