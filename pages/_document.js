fbdc1c99e4db31a23d913591497a3a37164cb38e
Lainey <lainey@infograte.com>
2022-09-10 14:42:28 -0400

    update secondary font

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&display=swap"
                         rel="stylesheet"
                     />
                     <link
